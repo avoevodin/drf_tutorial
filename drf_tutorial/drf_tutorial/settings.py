6a9574e6b010684954a3f60eed57fa1f4c1a219d
avo888 <avoevodin8888@gmail.com>
2021-10-26 00:38:04 +0300

    * updated with lesson 12 of drf #12

--- drf_tutorial/drf_tutorial/settings.py
+++ drf_tutorial/drf_tutorial/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'ckeditor_uploader',
     'django_filters',
     'djoser',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- drf_tutorial/drf_tutorial/urls.py
+++ drf_tutorial/drf_tutorial/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +29,8 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('api/v1/', include('movies.urls')),
 ]
+
+urlpatterns += doc_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
